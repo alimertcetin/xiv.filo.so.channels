7ab765f68a5ce6f55797bbbbf91794460659c690
alimertcetin <mrtmert54@gmail.com>
2025-09-10 18:31:59 +0300

    feat: Add StringChannelSO

---
+++ Runtime/StringChannelSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace XIV.Packages.ScriptableObjects.Channels
+{
+    /// <summary>
+    /// Channel for string values.
+    /// </summary>
+    [CreateAssetMenu(menuName = MenuPaths.CHANNEL_BASE_MENU + nameof(StringChannelSO))]
+    public class StringChannelSO : XIVChannelSO<string>
+    {
+        
+    }
+}
